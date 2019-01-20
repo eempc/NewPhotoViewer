19bbcafaf746ca5fbecf0381aaff4858241ad2f0
eempc <evc3355@gmail.com>
2019-01-20 22:28:33 +0000

    Basic image display. Image file check

--- NewPhotoViewer/Form1.cs
+++ NewPhotoViewer/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace NewPhotoViewer {
     public partial class Form1 : Form {
@@ -14,8 +15,44 @@ namespace NewPhotoViewer {
             InitializeComponent();
         }
 
-        public void OpenFile (string filePath) {
-            textBox1.Text = filePath;
+        string[] imageFiles = { ".jpg", ".png", ".jpeg", ".jpe", ".gif", ".bmp" };        
+        private Bitmap myImage;
+
+        // Opening start method from Program.cs if an image file was opened directly
+        public void StartMethod (string filePath) {
+            ShowImage(filePath);
+        }
+
+        public void OpenImage() {
+            // Create a string for the file dialogue filter based on the declared string array
+            string filterString = "Image files|";
+            foreach (string s in imageFiles) {
+                filterString += "*" + s + ";"; 
+            }
+            filterString = filterString.Remove(filterString.Length - 1);
+
+            // Open a file manually with the button
+            OpenFileDialog openFD = new OpenFileDialog();
+            openFD.Filter = filterString;
+            if (openFD.ShowDialog() == DialogResult.OK) {
+                ShowImage(openFD.FileName);
+            }
+        }
+
+        // Make picture box show an image, perform an extension beforehand
+        public void ShowImage(string filePath) {
+            foreach (string s in imageFiles) {
+                if (Path.GetExtension(filePath) == s) {
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    myImage = new Bitmap(filePath);
+                    pictureBox1.Image = (Image) myImage;
+                    break;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            OpenImage();
         }
     }
 }
--- NewPhotoViewer/Program.cs
+++ NewPhotoViewer/Program.cs
@@ -9,20 +9,21 @@ namespace NewPhotoViewer {
     static class Program {
         /// <summary>
         /// The main entry point for the application. Where you can load files by double clicking them.
-        /// Add string[] args to Main() then check it isn't empty and the file that triggered the app exists
+        /// Add string[] args to Main() then check it isn't empty and the file that triggered the app exists. I'm guessing the string refers to multiple files
         /// </summary>
         [STAThread]
         static void Main(string[] args) {
             if (args != null && args.Length > 0) { // Arguments found
-                string fileName = args[0];
+                string fileName = args[0]; // Picking the first file if multiple files are selected?
                 if (File.Exists(fileName)) { // Check file exists
                     Application.EnableVisualStyles();
                     Application.SetCompatibleTextRenderingDefault(false);
 
-                    Form1 MainFrom = new Form1();
-                    MainFrom.OpenFile(fileName);
-                    Application.Run(MainFrom);
-                } else { // If no file exists, launch app anyway
+                    // This is just how it works, this piece of code will do startup methods if launched externally
+                    Form1 MainForm = new Form1();
+                    MainForm.StartMethod(fileName); // StartMethod is run if the app was launched by clicking a file
+                    Application.Run(MainForm);
+                } else { // If no file exists, launch app anyway, this bit is probably redundant.
                     MessageBox.Show("No file exists", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
                     Application.EnableVisualStyles();
