050ff51518708dc3eb651e6e1878802d9ad81b6b
eempc <evc3355@gmail.com>
2019-01-21 17:26:58 +0000

    Zoom added

--- NewPhotoViewer/Form1.Designer.cs
+++ NewPhotoViewer/Form1.Designer.cs
@@ -28,14 +28,18 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonZoomIn = new System.Windows.Forms.Button();
+            this.buttonZoomOut = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(86, 73);
+            this.button1.Location = new System.Drawing.Point(3, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(44, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -43,55 +47,98 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(408, 73);
+            this.textBox1.Location = new System.Drawing.Point(53, 569);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(326, 144);
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(847, 44);
             this.textBox1.TabIndex = 1;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(157, 209);
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(53, 53);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(257, 127);
+            this.pictureBox1.Size = new System.Drawing.Size(847, 510);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(455, 271);
+            this.button2.Location = new System.Drawing.Point(906, 53);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(44, 23);
             this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
+            this.button2.Text = "Up";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(39, 254);
+            this.button3.Location = new System.Drawing.Point(3, 53);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(44, 23);
             this.button3.TabIndex = 4;
-            this.button3.Text = "button3";
+            this.button3.Text = "Down";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonZoomIn, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.buttonZoomOut, 2, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(953, 616);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // buttonZoomIn
+            // 
+            this.buttonZoomIn.Location = new System.Drawing.Point(3, 569);
+            this.buttonZoomIn.Name = "buttonZoomIn";
+            this.buttonZoomIn.Size = new System.Drawing.Size(44, 44);
+            this.buttonZoomIn.TabIndex = 5;
+            this.buttonZoomIn.Text = "Zoom in";
+            this.buttonZoomIn.UseVisualStyleBackColor = true;
+            this.buttonZoomIn.Click += new System.EventHandler(this.buttonZoomIn_Click);
+            // 
+            // buttonZoomOut
+            // 
+            this.buttonZoomOut.Location = new System.Drawing.Point(906, 569);
+            this.buttonZoomOut.Name = "buttonZoomOut";
+            this.buttonZoomOut.Size = new System.Drawing.Size(44, 44);
+            this.buttonZoomOut.TabIndex = 6;
+            this.buttonZoomOut.Text = "Zoom out";
+            this.buttonZoomOut.UseVisualStyleBackColor = true;
+            this.buttonZoomOut.Click += new System.EventHandler(this.buttonZoomOut_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(953, 616);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Evelyn\'s Replacement Picture Viewer";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -102,6 +149,9 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button buttonZoomIn;
+        private System.Windows.Forms.Button buttonZoomOut;
     }
 }
 
--- NewPhotoViewer/Form1.cs
+++ NewPhotoViewer/Form1.cs
@@ -13,30 +13,31 @@ namespace NewPhotoViewer {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+            tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.InsetDouble;
         }
 
         string[] imageExtensions = { ".jpg", ".png", ".jpeg", ".jpe", ".gif", ".bmp", ".tiff", ".tif" };        
-        private Bitmap myImage;
+        private Bitmap currentImage;
 
         // Opening start method from Program.cs if an image file was opened directly        
         int currentlyLoadedIndex;
         public void StartMethod (string filePath) {
             ShowImage(filePath);
             EnumerateFiles(filePath);
-            currentlyLoadedIndex = imageFileList.IndexOf(filePath);
         }
 
         // Enumerate files in directory
         List<string> imageFileList = new List<string>();
         public void EnumerateFiles(string filePath) {
-            string[] allFiles = Directory.GetFiles(Path.GetDirectoryName(filePath), "*");
+            string[] allFiles = Directory.GetFiles(Path.GetDirectoryName(filePath));
             foreach (string file in allFiles) {
-                if (IsImage(file)) {
-                    imageFileList.Add(file);
-                }
+                if (IsImage(file)) 
+                    imageFileList.Add(file);               
             }
+            currentlyLoadedIndex = imageFileList.IndexOf(filePath);
         }
 
+        // Do not use until files have been re-enumerated
         public void OpenImage() {
             // Create a string for the file dialogue filter based on the declared string array
             string filterString = "Image files|";
@@ -50,6 +51,7 @@ namespace NewPhotoViewer {
             openFD.Filter = filterString;
             if (openFD.ShowDialog() == DialogResult.OK) {
                 ShowImage(openFD.FileName);
+                EnumerateFiles(openFD.FileName);
             }
         }
 
@@ -57,10 +59,12 @@ namespace NewPhotoViewer {
         string currentlyLoadedFile;
         public void ShowImage(string filePath) {
             if (IsImage(filePath)) {
-                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-                myImage = new Bitmap(filePath);
-                pictureBox1.Image = (Image) myImage;
-                currentlyLoadedFile = filePath;                
+                pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+                currentImage = new Bitmap(filePath);
+                pictureBox1.Image = (Image) currentImage;
+                currentlyLoadedFile = filePath;
+                currentlyLoadedIndex = imageFileList.IndexOf(filePath);
+                currentZoomFactor = 1;
             }
         }
 
@@ -76,24 +80,52 @@ namespace NewPhotoViewer {
             return isImage;
         }
 
-
-
         private void button1_Click(object sender, EventArgs e) {
             OpenImage();
         }
 
+        // Diagnostic text box
         public void TextBoxDisplay(string someString) {            
-            textBox1.AppendText(DateTime.Now.ToString() + " " + someString + Environment.NewLine);
+            textBox1.AppendText(DateTime.Now.ToString() + " - " + someString + Environment.NewLine);
         }
 
+        // Next pic
         private void button2_Click(object sender, EventArgs e) {
-            ShowImage(imageFileList[(currentlyLoadedIndex + 1) % imageFileList.Count]); // % is an easier solution to deal with getting to the end of the list
-            currentlyLoadedIndex++;
+            ShowImage(imageFileList[(currentlyLoadedIndex + 1) % imageFileList.Count]); // Formula for rollover at > Count
         }
 
+        // Previous pic
         private void button3_Click(object sender, EventArgs e) {
-            ShowImage(imageFileList[(currentlyLoadedIndex - 1) % imageFileList.Count]); // % is an easier solution to deal with getting to the end of the list
-            currentlyLoadedIndex--;
+            ShowImage(imageFileList[(currentlyLoadedIndex + (imageFileList.Count - 1)) % imageFileList.Count]); // Formula for rollover at <0
+        }
+
+        double currentZoomFactor = 1;
+        double zoomStep = 0.1;
+        private void buttonZoomIn_Click(object sender, EventArgs e) {
+            if (currentZoomFactor < 2) {
+                //Size newSize = new Size((int) (currentImage.Width * (currentZoomFactor + zoomStep)), (int) (currentImage.Height * (currentZoomFactor + zoomStep)));
+                //Bitmap bmp = new Bitmap(currentImage, newSize);
+                //pictureBox1.Image = bmp;
+                //currentZoomFactor += zoomStep;
+                Zoom(1);
+            }
+        }
+
+        private void buttonZoomOut_Click(object sender, EventArgs e) {
+            if (currentZoomFactor > 0.1) {
+                //Size newSize = new Size((int) (currentImage.Width * (currentZoomFactor - zoomStep)), (int) (currentImage.Height * (currentZoomFactor - zoomStep)));
+                //Bitmap bmp = new Bitmap(currentImage, newSize);
+                //pictureBox1.Image = bmp;
+                //currentZoomFactor -= zoomStep;
+                Zoom(-1);
+            }
+        }
+
+        public void Zoom(int x) {
+            Size newSize = new Size((int) (currentImage.Width * (currentZoomFactor + zoomStep * x)), (int) (currentImage.Height * (currentZoomFactor + zoomStep * x)));
+            Bitmap bmp = new Bitmap(currentImage, newSize);
+            pictureBox1.Image = bmp;
+            currentZoomFactor += zoomStep *x;
         }
     }
 }
